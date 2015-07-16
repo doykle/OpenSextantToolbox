3eb213f19d62844961898aa851e553ecff3e2dd4
Andrew Gallant <andrew@diffeo.com>
2015-07-16 08:58:55 -0400

    Use the correct version of Java to run OpenSextant.

--- OpenSextantToolbox/scripts/start.sh
+++ OpenSextantToolbox/scripts/start.sh
@@ -3,4 +3,4 @@
 #export proxyhost=
 #export proxyport=
 
-java -Xmx8G -Dsolr.home=./solr -Dlog4j.configuration=file:./etc/log4j.properties -classpath ./lib/*:./lib/GATE/* org.opensextant.service.OpenSextantServer ./etc/service-config.properties
+$JAVA_HOME/bin/java -Xmx8G -Dsolr.home=./solr -Dlog4j.configuration=file:./etc/log4j.properties -classpath ./lib/*:./lib/GATE/* org.opensextant.service.OpenSextantServer ./etc/service-config.properties
